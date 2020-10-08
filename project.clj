0be7832aed8726f02dc37122062075c3e3268394
rik gee <ericgullufsen@gmail.com>
2020-10-07 20:57:18 -0800

    proof of concept for footy scores

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.8.2"]
-                 [ring/ring-jetty-adapter "1.8.2"]])
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [clj-http "3.10.3"]
+                 [org.clojure/data.json "0.2.7"]]
+  :main rikhw.core)
--- src/rikhw/core.clj
+++ src/rikhw/core.clj
@@ -1,10 +1,25 @@
 (ns rikhw.core
-  (:require [ring.util.response]))
+  (:require [ring.util.response]
+            [ring.adapter.jetty :as jetty]
+            [clojure.data.json  :as json]
+            [clj-http.client    :as client]))
 
-(defn foo
+(def comps ((json/read-str 
+   ((client/get "https://api.football-data.org/v2/competitions" 
+         {:headers {:X-AUTH-TOKEN "1a65e8acccdb47949431186d2d4ea406"}}) 
+    :body)) 
+  "competitions"))
+
+(def compnames (map (fn [c] (c "name")) comps))
+
+(defn handler
   "I don't do a whole lot."
   [req]
-  (ring.util.response/response "Hey (created with ring.util.response)")
+  (ring.util.response/response (str "Hey first comp is " (first compnames)))
   ;;{ :status 200 :headers {"content-type" "text/html"} :body "Hey"}
   ;;(ring.util.response/response "Hey man whatup...")
   )  
+
+(defn -main []
+  ;; run that server boi! port three stacks
+  (jetty/run-jetty handler {:port 3000}))
