e8f8f3876839b5e75d8fa99aecb0df80c3dec6e2
rik gee <ericgullufsen@gmail.com>
2020-10-07 21:50:52 -0800

    extended the poc

--- src/rikhw/core.clj
+++ src/rikhw/core.clj
@@ -4,18 +4,28 @@
             [clojure.data.json  :as json]
             [clj-http.client    :as client]))
 
-(def comps ((json/read-str 
-   ((client/get "https://api.football-data.org/v2/competitions" 
-         {:headers {:X-AUTH-TOKEN "1a65e8acccdb47949431186d2d4ea406"}}) 
-    :body)) 
-  "competitions"))
+(def base-url    "https://api.football-data.org/v2/")
+(def matches-url (str base-url "matches"))
+(def comps-url   (str base-url "competitions"))
+(def headersmap  {:headers {:X-AUTH-TOKEN "1a65e8acccdb47949431186d2d4ea406"}}) 
+
+(def comps 
+  ((json/read-str ((client/get comps-url headersmap) :body)) "competitions"))
+
+(defn get-games [] 
+  (let [resp (client/get matches-url headersmap)
+        bod  (resp :body)
+        jb   (json/read-str bod)
+        ms   (jb  "matches")]
+    ms
+))
 
 (def compnames (map (fn [c] (c "name")) comps))
 
 (defn handler
   "I don't do a whole lot."
   [req]
-  (ring.util.response/response (str "Hey first comp is " (first compnames)))
+  (ring.util.response/response (str (first (get-games))))
   ;;{ :status 200 :headers {"content-type" "text/html"} :body "Hey"}
   ;;(ring.util.response/response "Hey man whatup...")
   )  
