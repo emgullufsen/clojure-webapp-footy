395b7c8cc4434aba1e3bac5412479177dd8422a3
rickygee <eric@saltcakedsmokestacks.org>
2020-11-06 11:49:46 -0900

    CI stuff

--- test/rikhw/core_test.clj
+++ test/rikhw/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [rikhw.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def team-map {:homeTeam {:id 7} :awayTeam {:id 89}})
+
+(deftest get-home-id-check
+    (is (= (get-home-id team-map) 7)))
