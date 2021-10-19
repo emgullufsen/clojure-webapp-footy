b8651e828f33aae31e19fef712b239351156863c
rickygeeman <ricky@rickysquid.org>
2021-10-18 19:01:33 -0800

    content type try 3

--- src/rikhw/core.clj
+++ src/rikhw/core.clj
@@ -163,7 +163,7 @@
   [:#tomorrow] (html/set-attr :href (str "?" (codec/form-encode {:gameDate (add-day day)}))))
 
 (defn respond-with [mv gd]
-  (-> (ring.util.response/response (reduce str (matchesindex mv gd)))) (ring.util.response/header "Content-Type" "text/html; charset=utf-8"))
+  (ring.util.response/header (ring.util.response/response (reduce str (matchesindex mv gd))) "Content-Type" "text/html; charset=utf-8") 
 
 (defn handler
   "give client matches for the day"
